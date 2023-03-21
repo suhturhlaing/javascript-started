03a760217cc3483db328a0b607726b1ef82f9d0f
Su Htur Hlaing <shturhlaing@gmail.com>
2023-03-21 16:53:20 +0700

    [opreator create]

---
+++ DestructuringFunction/main.js
@@ -0,0 +1,26 @@
+// Destructuring with array
+
+const numbers = [1, 2, 3, 4, 5];
+
+// simple array 1
+// const x = numbers[0];
+// console.log(x);
+
+// simple array 2
+// const [x, y, z] = numbers;
+// console.log(x, y, z);
+
+
+// destucturing array
+// const [, , z] = numbers;
+// console.log(z);
+
+// destucturing array with rest parameter
+// const [x, y, ...z] = numbers;
+// console.log(x, y, z);
+
+    // or
+
+// const [, , ...z] = numbers;
+// console.log(z);
+
--- Operator/unaryOperator.js
+++ Operator/unaryOperator.js
@@ -1,23 +1,87 @@
-//  *** Unary Operator
+// ------------------------------  *** Unary Operator *** -----------------------------------
 
-// ** unary plus (+) **
-// const a = 10;
-// const b = -100;
-// const c = 43;
 
-// console.log(+a);
-// console.log(+b);
-// console.log(+c);
+// ------------------------------ Unary plus -------------------------------------
+const number = 10;
+console.log(+number);
 
-// ** unary operator (-) **
-// const x = 10;
-// const y = -100;
+const number1 = '50';
+console.log(+number1);
 
-// console.log(-x);
-// console.log(-y);
 
-// ** unary operator Logical ! (Not) **  || convert to boolean (true or false)
+// ------------------------------ Unary minus -------------------------------------
+const value = "10";
+console.log(-value);
 
-const m = true;
+const value1 = 20;
+console.log(-value1);
 
-console.log(-m);
+const value3 = "hello";
+console.log(-value3);
+
+// ------------------------------ Logical Not (!) -------------------------------------
+const logical = "10";
+console.log(!logical);      //output false
+
+const logical1 = true;
+console.log(!logical1);       //output false
+
+const logical2 = 30;
+console.log(!logical2);        //output false
+
+// or
+
+console.log(!!logical2);        //output true
+
+
+// ------------------------------ Bitwise NOT (~) -------------------------------------
+
+const bitwise = 6;
+console.log(~bitwise);       //output -7
+
+const bitwise1 = -9;
+console.log(~bitwise1);     //output 8
+
+
+
+
+// ------------------------------ Increment Operator(++) -------------------------------------
+let increase = 10;
+increase++;
+console.log(increase);      //output 11
+
+
+
+
+// ------------------------------ Decrement Operator(++) -------------------------------------
+
+let decrease = 10;
+decrease--;
+console.log(decrease);      //output 9
+
+
+
+// ------------------------------ void Operator -------------------------------------
+let myVoid = void function () {
+    return "hello";
+}
+console.log(myVoid);        //output undefined
+
+
+// ------------------------------ delete Operator -------------------------------------
+
+const myObject = {
+    name: "John",
+    age: 24
+}
+delete myObject.name;
+console.log(myObject);   //output { age: 24 }
+
+
+// ------------------------------ typeof Operator -------------------------------------
+
+const myString = "name"
+console.log(typeof (myString));         // output string
+
+const myNumber = 21;
+console.log(typeof (myNumber));         // output number
