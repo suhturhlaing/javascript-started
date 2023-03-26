47278d11dec8f1d7d88b115dd40562780193fa22
Su Htur Hlaing <shturhlaing@gmail.com>
2023-03-26 17:04:41 +0630

    [modify arithmetic]

---
+++ Operator/arithmeticOperator.js
@@ -0,0 +1,53 @@
+// ------------------------------  *** Arithmetic Operator *** ---------------------------------------
+
+
+// -----------------------   plus / addition (+) arithmetic Operator  ---------------------
+let x = 7, y = 5;
+
+console.log('x + y = ', x + y);         //output     x + y =  12
+
+
+
+// -----------------------   minus / substraction (-) arithmetic Operator  -------------------
+
+console.log("x - y = ", x - y);         //output     x - y =  2
+
+
+
+// -----------------------   mutliplication (*) arithmetic Operator  -------------------
+
+console.log("x * y = ", x * y);          //output    x * y =  35
+
+
+// -----------------------   division (/) arithmetic Operator  -------------------
+
+console.log(" x / y = ", x / y);            //output     x / y =  1.4
+
+
+// -----------------------   remainder (%) arithmetic Operator  -------------------
+
+console.log(" x % y = ", x % y);            //output     x % y =  2
+
+
+// -----------------------   increment (++) arithmetic Operator  -------------------
+
+console.log('++x =', ++x);          //output    ++x = 8  (x is now 8)
+
+console.log("x++ = ", x++);         //output    print 8 and then increase to 9
+
+console.log('x = ', x);             //output     x = 9  (now x is 9)
+
+
+
+// -----------------------   decrement (--) arithmetic Operator  -------------------
+
+console.log(" --x = ", --x);            //output     --x = 8  (x is now 8)
+
+console.log("x-- = ", x--);             //output     x-- = 8 (print 8 and decreased to 7);
+
+console.log("x = ", x);                 //output     x = 7  (now x is 7)
+
+
+// -----------------------   exponentiation (**) arithmetic Operator  -------------------
+
+console.log(" x ** y = ", x ** y);          //output     x ** y =  16807
